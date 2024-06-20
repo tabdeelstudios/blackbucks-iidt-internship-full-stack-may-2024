2240356c9a7eabef75b8830edd2a9663670c2a0d
Aquib Ajani <aquib@tabdeel.in>
2024-06-20 17:40:47 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,10 @@
+sales_data = [
+    {"item": "Laptop", "quantity": 4, "price_per_unit": 1000},
+    {"item": "Phone", "quantity": 10, "price_per_unit": 500},
+    {"item": "Tablet", "quantity": 5, "price_per_unit": 300},
+    {"item": "Monitor", "quantity": 3, "price_per_unit": 150},
+    {"item": "Keyboard", "quantity": 7, "price_per_unit": 50},
+    {"item": "Mouse", "quantity": 8, "price_per_unit": 25},
+    {"item": "Charger", "quantity": 15, "price_per_unit": 20},
+    {"item": "Headphones", "quantity": 6, "price_per_unit": 80},
+]
