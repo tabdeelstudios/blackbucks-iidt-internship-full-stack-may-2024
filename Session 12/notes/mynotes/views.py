9d01a78de0ff4e34ca87bc035d0b74d3ece595ed
Aquib Ajani <aquib@tabdeel.in>
2024-06-28 11:10:57 -0400

    Add files via upload

--- Session 12/notes/mynotes/views.py
+++ Session 12/notes/mynotes/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status, generics
+from datetime import datetime
 from .models import NoteModel
 from .serializers import NoteSerializer
 
@@ -39,3 +40,24 @@ class NoteDetail(generics.GenericAPIView):
         else:
             serializer = self.serializer_class(note)
             return Response({"status":"success", "data":{"note":serializer.data}}, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        note = self.get_note(pk=pk)
+        if note==None:
+            return Response({"status":"failed", f"message":"Note with ID:{pk} not found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.serializer_class(note, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.validated_data['updatedAt']=datetime.now()
+            serializer.save()
+            return Response({"status":"success", "data":{"note":serializer.data}}, status=status.HTTP_200_OK)
+        
+        return Response({"status":"failed", "errors":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        note = self.get_note(pk=pk)
+        if note==None:
+            return Response({"status":"failed", f"message":"Note with ID:{pk} not found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        note.delete()
+        return Response({"status":"deleted"}, status=status.HTTP_204_NO_CONTENT)
